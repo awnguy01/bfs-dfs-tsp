0a14e37d1c47352abae55d0d95439b5ebac4a5b3
Andrew Nguyen Vo <awnguy01@louisville.edu>
2019-09-09 19:38:41 -0400

    Final submission

--- utils.py
+++ utils.py
@@ -15,14 +15,6 @@ def get_file_path(file_types: Tuple[str, str] = []) -> str or None:
     return None
 
 
-def print_progress(progress_amount: int, total: int, percent_increments: int) -> None:
-    """Print a progress bar animation to the console"""
-    percentage: float = progress_amount / total * 100
-    fill_length: str = '=' * int(percentage // percent_increments)
-    empty_length: str = ' ' * (100 // percent_increments - len(fill_length))
-    print(f'[{fill_length}{empty_length}] {percentage:.2f}% complete', end='\r')
-
-
 def calc_distance(city_a: Node, city_b: Node) -> float:
     """Calculate distance between cities using distance formula"""
     return ((float(city_b.x) - float(city_a.x)) ** 2 + (float(city_b.y) - float(city_a.y)) ** 2) ** (1 / 2)
